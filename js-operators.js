a7f545373a7d93c0307c94f2df0b70f013c58115
LimaEchoEcho0123 <mmmyappers101@gmail.com>
2023-09-21 22:45:49 -0600

    week02

---
+++ js-operators.js
@@ -0,0 +1,62 @@
+// Addition
+
+// let y = 5;
+// let x = y + 2;
+// document.getElementById("demo").innerHTML = "Value of x is: " + x;
+
+
+// Multiplication
+
+// let y = 3;
+// let x = y * 2;
+// document.getElementById("demo").innerHTML = "Value for x: " + x;
+
+// Subtraction
+
+// let y = 3;
+// let x = y - 2;
+// document.getElementById("demo").innerHTML = "Value for x: " + x;
+
+// Concat
+
+// let text1 = "Good ";
+// let text2 = "Morning";
+// let text3 = text1 + text2;
+// document.getElementById("demo").innerHTML = text3;
+
+// Division and Variable Assignment
+
+// let x = 10;
+// let y = 5;
+// x /= y;
+// document.getElementById("demo").innerHTML = "Value of x is: " + x;
+
+// Incrementation to existing value
+
+// let x = 10;
+// let y = 3;
+// x += y;
+// document.getElementById("demo").innerHTML = "Value of x is: " + x;
+
+// Compares two values for equality (Strict equality)
+
+// let x = 5;
+// document.getElementById("demo").innerHTML = (x == 8);
+
+// Less than... check if one number is lower than another number
+
+// let x = 5;
+// document.getElementById("demo").innerHTML = (x < 8);
+
+// Not Equal... Compares two values if equal or not
+
+// let x = 5;
+// document.getElementById("demo").innerHTML = (x != 8);
+
+// Less than and greater than
+
+// let x = 6;
+// let y = 3;
+// document.getElementById("demo").innerHTML = 
+// (x < 10 && y > 0) + "<br>" +
+// (x < 10 && y < 0);
--- sandbox/debugging.js
+++ sandbox/debugging.js
@@ -30,13 +30,17 @@
 
 // solution
 
-const PI = 3.14
-let area = 0
-function circleArea(radius){
-    const area = radius * radius * PI;
-    return area
-}
-area = circleArea(3);
-console.log("Area1:", area)
-area = circleArea(4)
-console.log("Area2:", area)
+// const PI = 3.14
+// let area = 0
+// function circleArea(radius){
+//     const area = radius * radius * PI;
+//     return area
+// }
+// area = circleArea(3);
+// console.log("Area1:", area)
+// area = circleArea(4)
+// console.log("Area2:", area)
+
+let num1 = 100;
+let num2 = '10';
+let sum = parseInt(num1) + parseInt(num2);
