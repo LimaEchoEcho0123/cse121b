0d1db34692ae7982cab66c8df1ada69f37536d59
LimaEchoEcho0123 <mmmyappers101@gmail.com>
2023-09-22 17:07:53 -0600

    finished

---
+++ scripts/w02-task.js
@@ -0,0 +1,45 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullname = "Edwards Lee";
+const currentYear = new Date().getFullYear();
+const profilePicture = "images/me.jpg";
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullname}</strong>`;
+yearElement.textContent = `<strong>${currentYear}</strong>`;
+imageElement.setAttribute("src", profilePicture);
+// imageElement.setAttribute = `<strong>"Profile Image of ${profilePicture}"</strong.`;
+
+
+
+/* Step 5 - Array */
+let foodArray = ["sushi", "pad thai", "fried rice"];
+foodElement.textContent = foodArray;
+let anotherfood = "eggrolls";
+foodArray.push(anotherfood);
+foodElement.innerHTML += `<br>${foodArray}`;
+foodArray.shift();
+foodElement.innerHTML += `<br>${foodArray}`;
+foodArray.pop();
+foodElement.innerHTML += `<br>${foodArray}`;
+
+
+
+
+
+
+
